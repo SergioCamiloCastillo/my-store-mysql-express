ba7cf4b73d1bc669538987dae6049a760435ff7e
SergioCamiloCastillo <scastillonu@uniminuto.edu.co>
2021-12-03 22:53:59 -0500

    add foreign key

--- db/models/index.js
+++ db/models/index.js
@@ -1,9 +1,12 @@
 //envia la conexion hacia los modelos
 
 const { User, UserSchema } = require('./userModel');
+const { Customer, CustomerSchema } = require('./userModel');
 
 function setupModels(sequelize) {
   //init le dice que debe seguir el esquema declaraod en userModel.js y la configuracion
   User.init(UserSchema, User.config(sequelize));
+  Customer.init(CustomerSchema, Customer.config(sequelize));
+  Customer.associate(sequelize.models);
 }
 module.exports = setupModels;
